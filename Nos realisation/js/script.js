4cfd3a0cd213f707c7ea55064d05d1557d52e300
othmane-qr <othmane.qorqach@gmail.com>
2020-02-05 19:28:24 +0100

    add section réalisation et section client

---
+++ Nos realisation/js/script.js
@@ -0,0 +1,17 @@
+$('.client-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplaySpeed:1000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
